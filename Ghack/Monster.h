1660c376e42023909e8092bb8aa071f7bb3096f1
U-rogue\buble <buble@rogue.(none)>
2014-03-29 00:02:38 -0700

    monster infos non-persistent types - 3

--- Ghack/Monster.h
+++ Ghack/Monster.h
@@ -11,17 +11,97 @@ You should have received a copy of the GNU General Public License along with thi
 */
 #include "Monster.h"
 
-typedef struct {} monsterinfo_t; 
-__DefMonsterInfo(struct __struct_MonsterTypeInfo);
+/*** 
+//typedef struct {} monsterinfo_t;
+__Def(struct __struct_MonsterTypeInfo) 
+__DefMonsterInfo(struct __struct_MonsterTypeInfo)
+monsterinfo_t typeinfo;
+***/
+/*** 
+typedef monsterinfo_t; 
+typedef struct {} __struct_MonsterTypeInfo;
+__DefMonsterInfo(__struct_MonsterTypeInfo);
+***/
+
+typedef monsterinfo_t; 
+#define DefMonsterTypeInfo \
+	typedef monsterinfo_t \ 
+typedef struct {} __struct_MonsterTypeInfo \
+__DefMonsterInfo(__struct_MonsterTypeInfo)
+
+@interface MonsterTypeInfo : NSObject 
+{
+	monsterinfo_t _typeinfo;
+}
+@end
+
+#define DefMonsterBaseInfo \
+	typedef monsterinfo_t \ 
+typedef struct {} __struct_MonsterBaseInfo \
+__DefMonsterInfo(__struct_MonsterBaseInfo)
+
+@interface MonsterBaseInfo : NSObject 
+{
+	monsterinfo_t _baseinfo;
+}
+@end
+
+#define DefMonsterManaInfo \
+	typedef monsterinfo_t \ 
+typedef struct {} __struct_MonsterManaInfo \
+__DefMonsterInfo(__struct_MonsterManaInfo)
+
+@interface MonsterManaInfo : NSObject 
+{
+	monsterinfo_t _manainfo;
+}
+@end
+
+#define DefMonsterPriestInfo \
+	typedef monsterinfo_t \ 
+typedef struct {} __struct_MonsterPriestInfo \
+__DefMonsterInfo(__struct_MonsterPriestInfo)
+
+@interface MonsterPriestInfo : NSObject 
+{
+	monsterinfo_t _priestinfo;
+}
+@end
+
+#define DefMonsterLevelInfo \
+	typedef monsterinfo_t \ 
+typedef struct {} __struct_MonsterLevelInfo \
+__DefMonsterInfo(__struct_MonsterLevelInfo)
+
+@interface MonsterLevelInfo : NSObject 
+{
+	monsterinfo_t _levelinfo;
+}
+@end
+
+#define DefMonsterEquipmentInfo \
+	typedef monsterinfo_t \ 
+typedef struct {} __struct_MonsterEquipmentInfo \
+__DefMonsterInfo(__struct_MonsterEquipmentInfo)
+
+@interface MonsterEquipmentInfo : NSObject 
+{
+	monsterinfo_t _equipmentinfo;
+}
+@end
+
 
 @interface Monster : NSObject 
 {
 
-	monsterinfo_t typeinfo;
+	monsterinfo_t _baseinfo;
+	monsterinfo_t _manainfo;
+	monsterinfo_t _priestinfo;
+	monsterinfo_t _levelinfo;
+	monsterinfo_t _equipmentinfo;
 
 }
-
-- (id)init;
-
+/*- (id)init;
+*/
 @end
 
