c982f74647dd505969d4c2b5355b6fe66135bb88
U-rogue\buble <buble@rogue.(none)>
2014-03-28 20:59:14 -0700

    cleanup

--- Ghack/MonsterInfo.h
+++ Ghack/MonsterInfo.h
@@ -1,8 +1,7 @@
 #import <Foundation/Foundation.h>
 #import <AppKit/AppKit.h>
-//#include "ghack.h"
 /*
-Copyright (C) 2013 v-zor
+Copyright (C) 2013 enrytheermit 
 
 This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation; either version 2 of the License, or (at your option) any later version.
 
